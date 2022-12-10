12bb45f7413647bfe0537852f94e0e81233f598a
Matvey Alekseev <m.alekseev@seowork.ru>
2022-12-11 01:08:11 +0300

    small fixes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import { Logger } from '@nestjs/common';
 import { clc } from '@nestjs/common/utils/cli-colors.util';
 import { ZmqSubServer } from './zmq';
-
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
